94e97763db91d8ea90ee6f341d9b682522b86471
mohammedazman <mohammedazman@users.noreply.github.com>
2022-08-06 20:17:38 +0000

    Fix styling

--- config/tamara.php
+++ config/tamara.php
@@ -3,8 +3,8 @@
 // config for AlazziAz/Tamara
 return [
     'uri' => env('TAMARA_URI', 'https://api-sandbox.tamara.co'),
-    'token' => env('TAMARA_TOKEN',''),
-    'notification_token' => env('TAMARA_NOTIFICATION_TOKEN',''),
+    'token' => env('TAMARA_TOKEN', ''),
+    'notification_token' => env('TAMARA_NOTIFICATION_TOKEN', ''),
     'request_timeout' => env('TAMARA_REQUEST_TIMEOUT', 10),
     'transport' => null,
     'success_url' => '',
